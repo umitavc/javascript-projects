7d5d83e75086e1e43bc45722f1db96f8ce075190
umitavc <umitavc62@gmail.com>
2023-03-13 16:09:26 +0300

    recipe proje ct v3
    beğenme özelliği eklendi içerik detay sayfası eklendi
    translate eklendi
    readme kısmı eklendi

--- 15- recipe-project/app.js
+++ 15- recipe-project/app.js
@@ -1,6 +1,12 @@
 
-const meals = document.getElementById('meals');
-const favoriteContainer = document.getElementById("fav-meals")
+const mealsEl = document.getElementById('meals');
+const favoriteContainer = document.getElementById("fav-meals");
+const mealInfoEl = document.getElementById("meal-info");
+const searchTerm = document.getElementById('search-term');
+const searchBtn = document.getElementById('search');
+const mealPopup = document.getElementById('meal-popup');
+const popupCloseBtn = document.getElementById('close-popup');
+
 
 getRandomMeal();
 fetchFavMeals();
@@ -28,10 +34,15 @@ async function getMealById(id) {
     return meal;
 }
 
-async function getMealsBySearch(terms) {
+async function getMealsBySearch(term) {
     const resp = await fetch(
         "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
     );
+
+    const respData = await resp.json();
+    const meals = respData.meals;
+
+    return meals;
 }
 
 function addMeal(mealData, random = false) {
@@ -50,7 +61,7 @@ function addMeal(mealData, random = false) {
     <div class="meal-body">
         <h4>${mealData.strMeal}</h4>
         <button class="fav-btn ">
-            <i class="fa fa-heart"></i>
+            <i class="fa fa-heart"></i>  
         </button>
     </div>
 
@@ -66,12 +77,13 @@ function addMeal(mealData, random = false) {
             addMealLs(mealData.idMeal);
             btn.classList.add("active")
         }
-        favoriteContainer.innerHTML ="";
         fetchFavMeals();
     });
 
-
-    meals.appendChild(meal);
+    meal.addEventListener('click', () => {
+        showMealInfo(mealData);
+    })
+    mealsEl.appendChild(meal);
 }
 
 
@@ -97,19 +109,20 @@ function getMealLs() {
 }
 
 
- async function fetchFavMeals() {
+async function fetchFavMeals() {
+    favoriteContainer.innerHTML = "";
     const mealIds = getMealLs();
 
-    const meals =[];
+    const meals = [];
 
-    for (let i =0; i< mealIds.length; i++){
+    for (let i = 0; i < mealIds.length; i++) {
         const mealId = mealIds[i];
         meal = await getMealById(mealId);
-       addMealFav(meal);
+        addMealFav(meal);
 
     }
 
-   
+
 }
 
 
@@ -117,7 +130,7 @@ function getMealLs() {
 function addMealFav(mealData,) {
 
     const favMeal = document.createElement('li');
-    
+
     favMeal.innerHTML = `
     <img
         src="${mealData.strMealThumb}"
@@ -125,6 +138,78 @@ function addMealFav(mealData,) {
     /><span>${mealData.strMeal}</span>
     <button class="clear"><i class="fas fa-window-close"></i></button>
 `;
-favoriteContainer.appendChild(favMeal);
+    const btn = favMeal.querySelector('.clear');
+    btn.addEventListener("click", () => {
+        removeMealLs(mealData.idMeal);
+
+        fetchFavMeals();
+    })
+    favoriteContainer.appendChild(favMeal);
 
 }
+
+function showMealInfo(mealData) {
+    // temizle
+    mealInfoEl.innerHTML = "";
+
+    // Yemek bilgilerini güncelle
+    const mealEl = document.createElement("div");
+
+    const ingredients = [];
+
+    // malzeme sayısını belirle
+    for (let i = 1; i <= 20; i++) {
+        if (mealData["strIngredient" + i]) {
+            ingredients.push(
+                `${mealData["strIngredient" + i]} - ${mealData["strMeasure" + i]
+                }`
+            );
+        } else {
+            break;
+        }
+    }
+
+    mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients
+            .map(
+                (ing) => `
+            <li>${ing}</li>
+            `
+            )
+            .join("")}
+        </ul>
+    `;
+
+    mealInfoEl.appendChild(mealEl);
+
+
+    mealPopup.classList.remove("hidden");
+}
+
+searchBtn.addEventListener("click", async () => {
+
+    mealsEl.innerHTML = "";
+
+    const search = searchTerm.value;
+    const meals = await getMealsBySearch(search);
+
+    if (meals) {
+        meals.forEach((meal) => {
+            addMeal(meal);
+        });
+    }
+});
+
+popupCloseBtn.addEventListener("click", () => {
+    mealPopup.classList.add("hidden");
+});
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
  ## 14 - Quiz Project
 ![Ekran görüntüsü 2023-03-06 121352](https://user-images.githubusercontent.com/62944836/223066929-188f5fdb-048e-4b11-b71f-e8d5c0ddd43f.png)
+
+## 15 - Recipe Project
+![Ekran görüntüsü 2023-03-13 160253](https://user-images.githubusercontent.com/62944836/224709811-2886f5c7-597d-4062-9a22-918363a41d13.png)
